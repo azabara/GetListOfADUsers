9de9ddf279fb4b2074c4d476e3830595864bc401
azabara <46056342+azabara@users.noreply.github.com>
2021-04-29 13:54:59 +0200

    Add files via upload

---
+++ GetListOfUsers.ps1
@@ -0,0 +1,20 @@
+﻿Clear-Host
+echo "Here is the list of all users:"
+Get-ADUser -Filter * | Format-Table -Property DistinguishedName,Name,Surname,ObjectClass,UserPrincipalName 
+$OU = Read-Host "You can enter specific OU name and i will check it for you"
+DO {
+if($? -eq $OU){
+    $ErrorActionPreference = "SilentlyContinue"
+    Get-ADUser -Filter * -SearchBase “ou=$OU,dc=eleks,dc=local" | Format-Table -Property DistinguishedName,Name,Surname,ObjectClass,UserPrincipalName
+    }
+else{
+    $ErrorActionPreference = "SilentlyContinue"
+    echo "Didn't find specified OU..."
+    $OU = Read-Host "Please try again or press 'Enter' to exit"
+    Get-ADUser -Filter * -SearchBase “ou=$OU,dc=eleks,dc=local”
+}
+}
+until($? -eq $OU)
+
+
+
